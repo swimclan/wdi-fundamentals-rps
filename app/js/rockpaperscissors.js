c9d54dc034451902e367ee2c2c550f0df0bf9834
Matt Herron <matthew.herron77@gmail.com>
2015-06-03 22:47:34 -0500

    winner logic

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -40,7 +40,15 @@ function getWinner(playerMove,computerMove) {
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
+    if (playerMove === computerMove) {
+        winner = 'tie';
+    } else if (playerMove === 'rock') {
+        computerMove === 'paper' ? winner = 'computer' : winner = 'player';
+    } else if (playerMove === 'paper') {
+        computerMove === 'scissors' ? winner = 'computer' : winner = 'player';
+    } else {
+        computerMove === 'rock' ? winner = 'computer' : winner = 'player';
+    }
     return winner;
 }
 
